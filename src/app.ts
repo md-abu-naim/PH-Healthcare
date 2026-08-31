3aabc5b6919356126c403627a351e03241fa5ab9
abunaim34 <farhanadnanfarabi@gmail.com>
2026-08-31 20:54:49 +0600

    Creating APIs For Creating Prescription And Getting Single Prescription

--- src/app.ts
+++ src/app.ts
@@ -13,6 +13,9 @@ import { UserRoutes } from "./app/module/user/user.route";
 import { getBkashIdToken } from "./app/lib/bkash";
 import { AppointmentRoutes } from "./app/module/appointment/appointment.route";
 import { DoctorRoutes } from "./app/module/doctor/doctor.route";
+import { PrescriptionRoutes } from "./app/module/prescription/prescription.route";
+import { PaymentRoutes } from "./app/module/payment/payment.route";
+import { ScheduleRoutes } from "./app/module/schedule/schedule.route";
 
 const app: Application = express();
 
@@ -34,6 +37,9 @@ app.use("/api/v1/auth", AuthRoutes);
 app.use("/api/v1/user", UserRoutes);
 app.use("/api/v1/appointment", AppointmentRoutes);
 app.use("/api/v1/doctor", DoctorRoutes);
+app.use("/api/v1/payment", PaymentRoutes);
+app.use("/api/v1/prescription", PrescriptionRoutes);
+app.use("/api/v1/schedule", ScheduleRoutes);
 
 app.get("/test", async (req: Request, res: Response, next: NextFunction) => {
 	try {
