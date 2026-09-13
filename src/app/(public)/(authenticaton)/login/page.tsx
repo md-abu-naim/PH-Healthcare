40a0db014cc44c96048f7033b19c2d2412f81fd2
abunaim34 <farhanadnanfarabi@gmail.com>
2026-09-13 19:18:35 +0600

    Getting started with tanstack form

--- src/app/(public)/(authenticaton)/login/page.tsx
+++ src/app/(public)/(authenticaton)/login/page.tsx
@@ -1,11 +1,30 @@
-import React from 'react';
+import LoginForm from "@/components/Form/LoginForm";
+import { GalleryVerticalEnd } from "lucide-react";
+import Image from "next/image";
+import Link from "next/link";
 
-const Login = () => {
-    return (
-        <div>
-            login
+export default function LoginPage() {
+  return (
+    <div className="grid min-h-svh lg:grid-cols-2">
+      <div className="flex flex-col gap-4 p-6 md:p-10">
+        <div className="flex justify-center gap-2 md:justify-start">
+          <Link href="/" className="flex items-center gap-2 font-medium">
+            PH Healthcare
+          </Link>
         </div>
-    );
-};
-
-export default Login;
+        <div className="flex flex-1 items-center justify-center">
+          <div className="w-full max-w-xs">
+            <LoginForm />
+          </div>
+        </div>
+      </div>
+      <div className="relative hidden bg-muted lg:block">
+        <Image width={500} height={500}
+          src="/login.jpg"
+          alt="Image"
+          className="absolute inset-0 h-full w-full object-cover dark:brightness-[0.2] dark:grayscale"
+        />
+      </div>
+    </div>
+  );
+}
--- src/app/(public)/(marketing)/layout.tsx
+++ src/app/(public)/(marketing)/layout.tsx
@@ -1,10 +1,14 @@
 
-const MarketingLayout = () => {
-    return (
-        <div>
-            layut
-        </div>
-    );
-};
+import Footer from "@/components/layout/public/Footer";
+import Header from "@/components/layout/public/Header";
+import { ReactNode } from "react";
 
-export default MarketingLayout;
+export default function layout({ children }: { children: ReactNode }) {
+  return (
+    <div className="flex flex-col min-h-screen">
+      <Header />
+      <main className="flex-1">{children}</main>
+      <Footer />
+    </div>
+  );
+}
